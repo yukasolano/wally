f8ceda8335e879e32573f6a60078ea8ea9190896
Rafael Ribeiro <tecinfo.rafael@gmail.com>
2019-10-23 01:25:38 -0300

    WALLY-4 - Adicionando banco em memória H2 para realização dos testes de integração, evitando usar um banco externo para realização dos testes.

--- src/test/java/com/warren/wally/db/WallyTestCase.java
+++ src/test/java/com/warren/wally/db/WallyTestCase.java
@@ -1,18 +1,15 @@
 package com.warren.wally.db;
 
-import com.warren.wally.WallyApplication;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Classe base para a execução de testes que requerem fazer uso dos beans do Spring, bem como ter acesso ao banco
  * de dados e demais configurações da aplicação.
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {WallyApplication.class})
-@TestPropertySource(locations = "/application.properties")
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class WallyTestCase {
 
 }
